3c060c3021b1e125a02e83e05afb551c02c05d0c
hiteshjogiya <hiteshjogiya2017@gmail.com>
2021-04-18 16:28:50 +0530

    add file

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthGuard } from './auth.guard';
+import { BookComponent } from './book/book.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { SignupComponent } from './signup/signup.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"",redirectTo:"/login",pathMatch:"full"},
+  {path:"login",component:LoginComponent},
+  {path:"register",component:RegisterComponent},
+  {path:"book",component:BookComponent,canActivate:[AuthGuard]},
+  {path:"up" ,component:SignupComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
